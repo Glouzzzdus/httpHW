c397e1666c9d54ff2c80ab8515b0e76c9dfeeb88
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2024-03-03 21:57:56 +0100

    Task 3 is implemented

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-class Program
+class Client
 {
-    static readonly HttpClient client = new HttpClient { Timeout = TimeSpan.FromSeconds(40) };
+    static readonly HttpClient client = new HttpClient();
 
     static async Task Main()
     {
@@ -14,7 +15,8 @@ class Program
             "http://localhost:8888/Success/",
             "http://localhost:8888/Redirection/",
             "http://localhost:8888/ClientError/",
-            "http://localhost:8888/ServerError/"
+            "http://localhost:8888/ServerError/",
+            "http://localhost:8888/MyNameByHeader/"
         };
 
         foreach (var url in urls)
@@ -23,12 +25,18 @@ class Program
             {
                 HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
-                Console.WriteLine(url + " " + (int)response.StatusCode + " " + response.StatusCode);
+                Console.WriteLine(url + " " + (int)response.StatusCode + " " + response.StatusCode); // Print url and status code
+
+                if (response.Headers.Contains("X-MyName")) // If header "X-MyName" is available
+                {
+                    var myName = response.Headers.GetValues("X-MyName").First();
+                    Console.WriteLine("My Name is: " + myName); // Print the value of header
+                }
             }
             catch (HttpRequestException e)
             {
-                Console.WriteLine("Exception Caught!");
-                Console.WriteLine($"Failed at fetching {url}. Message :{e.Message}");
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
             }
         }
     }
--- Listener/Program.cs
+++ Listener/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net;
-using System.Threading.Tasks;
 
 public class Listener
 {
@@ -11,58 +10,53 @@ public class Listener
         listener.Prefixes.Add(prefix);
         listener.Start();
         Console.WriteLine("Listening...");
-        Task.Run(() => StartListener(listener));
-        Console.ReadLine();
-    }
 
-    public static async void StartListener(HttpListener listener)
-    {
         while (true)
         {
-            HttpListenerContext context = await listener.GetContextAsync();
-            await Task.Run(() => ProcessRequest(context));
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string responseString;
+
+            switch (request.Url.AbsolutePath)
+            {
+                case "/Information/":
+                    response.StatusCode = 100;
+                    responseString = "Information!";
+                    break;
+                case "/Success/":
+                    response.StatusCode = 200;
+                    responseString = "Success!";
+                    break;
+                case "/Redirection/":
+                    response.StatusCode = 300;
+                    responseString = "Redirection!";
+                    break;
+                case "/ClientError/":
+                    response.StatusCode = 400;
+                    responseString = "Client Error!";
+                    break;
+                case "/ServerError/":
+                    response.StatusCode = 500;
+                    responseString = "Server Error!";
+                    break;
+                case "/MyNameByHeader/":
+                    response.StatusCode = 200;
+                    response.Headers.Add("X-MyName", "YourName");
+                    responseString = "Name is added in the header!";
+                    break;
+                default:
+                    response.StatusCode = 404;
+                    responseString = "Not Found!";
+                    break;
+            }
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
         }
-    }
-
-    public static void ProcessRequest(HttpListenerContext context)
-    {
-        HttpListenerRequest request = context.Request;
-        HttpListenerResponse response = context.Response;
-
-        string responseString;
-
-        switch (request.Url.AbsolutePath)
-        {
-            case "/Information/":
-                response.StatusCode = 100;
-                responseString = "Information!";
-                break;
-            case "/Success/":
-                response.StatusCode = 200;
-                responseString = "Success!";
-                break;
-            case "/Redirection/":
-                response.StatusCode = 300;
-                responseString = "Redirection!";
-                break;
-            case "/ClientError/":
-                response.StatusCode = 400;
-                responseString = "Client Error!";
-                break;
-            case "/ServerError/":
-                response.StatusCode = 500;
-                responseString = "Server Error!";
-                break;
-            default:
-                response.StatusCode = 404;
-                responseString = "Not Found!";
-                break;
-        }
-
-        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-        response.ContentLength64 = buffer.Length;
-        System.IO.Stream output = response.OutputStream;
-        output.Write(buffer, 0, buffer.Length);
-        output.Close();
     }
 }
